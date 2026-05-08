81952a969f29ddb00e6f8ee57d41569e9b9659cd
csharp-maxxer <valentin.haensler@posteo.de>
2026-05-08 12:23:12 +0200

    added folderstructure
    added the folders assets and pages:
    added 1 element each because you cant push emthy folders to git.

---
+++ Src/pages/choose_difficulty.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _5_Jahre_Hoelle.pages
+{
+    /// <summary>
+    /// Interaktionslogik für choose_difficulty.xaml
+    /// </summary>
+    public partial class choose_difficulty : Page
+    {
+        public choose_difficulty()
+        {
+            InitializeComponent();
+        }
+    }
+}
