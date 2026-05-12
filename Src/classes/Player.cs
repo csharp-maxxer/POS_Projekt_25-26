0c5f095d78b79371d7367c785e33195e3e375c34
Push-It-Dave <jaeger.david1008@gmail.com>
2026-05-12 15:17:33 +0200

    Class Player completed but not Attack this will be done later.

--- Src/classes/Player.cs
+++ Src/classes/Player.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,30 @@ namespace _5_Jahre_Hoelle.classes
 {
     class Player
     {
+        public List<Bullets> Bullets;
+        public double Damage {  get; set; }
+        public double Speed { get; set; }
+        public double Range { get; set; }
+        public int Grade { get; set; }
+        public double Energy { get; set; }
+        public double Firerate { get; set; }
+        public int X_Pos { get; set; }
+        public int Y_Pos { get; set; }
+
+        Player(double damage, double speed, double ramge, double firerate,int grade,double energy)
+        {
+            Damage = damage;
+            Speed = speed;  
+            Range = ramge;
+            Firerate = firerate;
+            Grade = grade;
+            Energy = energy;
+        }
+
+        
+        public void Attack()
+        {
+
+        }
     }
 }
