1f59e149ad386a3e749a07cfb2afbbd69aa24b2a
csharp-maxxer <valentin.haensler@posteo.de>
2026-05-12 15:26:11 +0200

    Added Rooms
    Added the base of the Rooms class

---
+++ Src/classes/Room.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_Jahre_Hoelle.classes
+{
+    public class Room
+    {
+        // 14x7 mit je 120px
+        private List<List<char>> room_matrix {  get; set; }
+        //public List<enemy> { get; private set; }
+        public bool IsCleared { get; private set; } 
+
+        Room()
+        {
+            IsCleared = false;
+        }
+
+
+    }
+}
