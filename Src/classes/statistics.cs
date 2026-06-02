b174e212c0c4a10e1bcf2b0a4c4185cc04fe6f8b
PUSH-IT-DAVE <jaeger.david1008@gmail.com>
2026-06-02 15:19:14 +0200

    Now when there is no file or the file is empty a correct one is created

--- Src/classes/statistics.cs
+++ Src/classes/statistics.cs
@@ -1,12 +1,116 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Text.Json;
+using System.Windows;
 
 namespace _5_Jahre_Hoelle.classes
 {
-    internal class statistics
+    public static class statistics
     {
+        private static int _attemps;
+        private static int _wins;
+        private static int _achievment_numer;
+
+        private static string filePath = "saves/statistics.json";
+
+        public static int Attemps
+        {
+            get
+            {
+                return _attemps;
+            }
+            set
+            {
+                _attemps = value;
+                Save();
+            }
+        }
+
+        public static int Wins
+        {
+            get
+            {
+                return _wins;
+            }
+            set
+            {
+                _wins = value;
+                Save();
+            }
+        }
+
+        public static int Achievments
+        {
+            get
+            {
+                return _achievment_numer;
+            }
+            set
+            {
+                _achievment_numer = value;
+                Save();
+            }
+        }
+
+        public static void init()
+        {
+            Load();
+        }
+
+        private static void Save()
+        {
+            Dictionary<string, int> data = new Dictionary<string, int>();
+
+            data.Add("Attemps", _attemps);
+            data.Add("Wins", _wins);
+            data.Add("Achievments", _achievment_numer);
+
+            string json = JsonSerializer.Serialize(data);
+
+            using (StreamWriter writer = new StreamWriter(filePath))
+            {
+                writer.Write(json);
+            }
+        }
+
+        private static void Load()
+        {
+            if (!File.Exists(filePath))
+            {
+                MessageBox.Show("File Gibt es nicht");
+                return;
+            }
+
+            string json;
+
+            using (StreamReader reader = new StreamReader(filePath))
+            {
+                json = reader.ReadToEnd();
+            }
+
+            Dictionary<string, int> data = JsonSerializer.Deserialize<Dictionary<string, int>>(json);
+
+            if (data == null)
+            {
+                MessageBox.Show("File ist leer");
+                return;
+            }
+
+            if (data.ContainsKey("Attemps"))
+            {
+                _attemps = data["Attemps"];
+            }
+
+            if (data.ContainsKey("Wins"))
+            {
+                _wins = data["Wins"];
+            }
+
+            if (data.ContainsKey("Achievments"))
+            {
+                _achievment_numer = data["Achievments"];
+            }
+        }
     }
 }
