dbe54ba5664ac3d091235d4f0f04d0d5f4a15908
PUSH-IT-DAVE <jaeger.david1008@gmail.com>
2026-06-04 12:28:15 +0200

    Energy Display UC was created

---
+++ Src/usercontrols/EnergyDisplay.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _5_Jahre_Hoelle.usercontrols
+{
+    /// <summary>
+    /// Interaktionslogik für EnergyDisplay.xaml
+    /// </summary>
+    public partial class EnergyDisplay : UserControl
+    {
+        public EnergyDisplay()
+        {
+            InitializeComponent();
+        }
+    }
+}
