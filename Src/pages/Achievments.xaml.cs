86d4c36e756fbe30a4f66ff354e66fa74a3b31f8
PUSH-IT-DAVE <jaeger.david1008@gmail.com>
2026-06-05 16:50:43 +0200

    Acheavments class is done and works excactly the same like the statistic one

--- Src/pages/Achievments.xaml.cs
+++ Src/pages/Achievments.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using _5_Jahre_Hoelle.classes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,35 @@ namespace _5_Jahre_Hoelle.pages
         public Achievments()
         {
             InitializeComponent();
+            Acheavments.init();
+            
+
+            if (Acheavments.Acheavment1 == 1)
+            {
+                Icon_attemps.Text = "☑";
+            }
+            else
+            {
+                Icon_attemps.Text = "☐";
+            }
+
+            if (Acheavments.Acheavment2 == 1)
+            {
+                Icon_Wins.Text = "☑";
+            }
+            else
+            {
+                Icon_Wins.Text = "☐";
+            }
+
+            if (Acheavments.Acheavment3 == 1)
+            {
+                Icon_acheavments.Text = "☑";
+            }
+            else
+            {
+                Icon_acheavments.Text = "☐";
+            }
         }
 
         private void Button_Zurück_Click(object sender, RoutedEventArgs e)
