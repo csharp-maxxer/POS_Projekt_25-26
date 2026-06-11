6f2d87a588d6ed28916b936930dc84787b4ba897
csharp-maxxer <valentin.haensler@posteo.de>
2026-06-11 15:59:15 +0200

    Create GameTracer.cs
    Added the GameTracer class, which tracks the current floor of the player, so each floor will spawn more rooms then the one before

---
+++ Src/classes/GameTracer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace _5_Jahre_Hoelle.classes
+{
+    public static class GameTracer
+    {
+        public static int LevelNumber { private get; set; }
+
+        public static int RoomsNumber
+        {
+            get
+            {
+                if (LevelNumber == 1)
+                    return 4;
+                if (LevelNumber == 2)
+                    return 7;
+                if (LevelNumber == 3)
+                    return 9;
+                if (LevelNumber == 4)
+                    return 10;
+                if (LevelNumber == 5)
+                    return 11;
+                MessageBox.Show("error, keine stage existiert");
+                return -1;
+            }
+            private set
+            {
+                
+            }
+        }
+    }
+}
