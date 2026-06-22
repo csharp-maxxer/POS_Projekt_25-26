c334f9b6ced703c95e2550f356d059092834b672
PUSH-IT-DAVE <jaeger.david1008@gmail.com>
2026-06-22 17:46:21 +0200

    bullet class is now Logged

--- Src/classes/Bullets.cs
+++ Src/classes/Bullets.cs
@@ -10,10 +10,10 @@ namespace _5_Jahre_Hoelle.classes
 {
     public class Bullets
     {
-        public Vector Direction {  get; set; }
+        public Vector Direction { get; set; }
         public double Damage { get; private set; }
 
-        public double Speed {  get; private set; }
+        public double Speed { get; private set; }
         public double Range { get; private set; }
         public double X_pos { get; set; }
 
@@ -23,11 +23,13 @@ namespace _5_Jahre_Hoelle.classes
 
         public double Y_pos_created { get; set; }
 
-        public int random {  get; set; }
+        public int random { get; set; }
         public double rotation { get; set; }
 
         public Bullets(double damage, double speed, double range, Vector direction, double x_create, double y_created)
         {
+            Logger.logger.Debug("bullet gets created");
+
             Random random_ = new Random();
 
             random = random_.Next(1, 3);
@@ -39,10 +41,15 @@ namespace _5_Jahre_Hoelle.classes
             Direction = direction;
             X_pos_created = x_create;
             Y_pos_created = y_created;
+
+            Logger.logger.Debug("bullet stats got set");
         }
 
-        public Bullets() { }
+        public Bullets()
+        {
+            
+        }
 
 
-    } 
+    }
 }
