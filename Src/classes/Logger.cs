d55e390becb0ff0e706a63e4904f8227c6357a85
PUSH-IT-DAVE <jaeger.david1008@gmail.com>
2026-06-22 14:19:12 +0200

    Game Page is now Logged

--- Src/classes/Logger.cs
+++ Src/classes/Logger.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Serilog;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,25 @@ using System.Threading.Tasks;
 
 namespace _5_Jahre_Hoelle.classes
 {
-    internal class Logger
+    public static class Logger
     {
+        public static Serilog.Core.Logger logger { get; private set; }
+        public static bool Initialized { get; private set; } = false;
+        public static void init(string logfilename)
+        {
+
+            logger = new LoggerConfiguration()
+               .MinimumLevel.Debug()
+               .WriteTo.Console()
+               .WriteTo.File(logfilename,
+                   rollingInterval: RollingInterval.Day,
+                   retainedFileCountLimit: 7)
+               .CreateLogger();
+
+
+            Log.Information("Logger initialisiert.");
+            Initialized = true;
+        }
+
     }
 }
