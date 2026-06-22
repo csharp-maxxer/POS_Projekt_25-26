c5dbeb9a8dde80910f4e1fffc865fbf3fcc7a047
PUSH-IT-DAVE <jaeger.david1008@gmail.com>
2026-06-22 17:30:40 +0200

    Pausescreen Usercontrol is now Logged

--- Src/usercontrols/Pausescreen.xaml.cs
+++ Src/usercontrols/Pausescreen.xaml.cs
@@ -32,17 +32,23 @@ namespace _5_Jahre_Hoelle.usercontrols
         public Pausescreen()
         {
             InitializeComponent();
+            Logger.logger.Debug("Pausescreen initialized");
         }
         private string _savedata;
 
         public Pausescreen(string savedata)
-        { 
+        {
             InitializeComponent();
+            Logger.logger.Debug("Pausescreen initialized");
+
             _savedata = savedata;
+            Logger.logger.Debug("Save data was loaded in Pausescreen");
         }
 
         private void Button_continue_Click(object sender, RoutedEventArgs e)
         {
+            Logger.logger.Debug("button Cntinue was pressed");
+
             // cbatgpt anfang: Mach mir den ContinueButton das er funktioniert(code)
             ContinueClicked?.Invoke();
 
@@ -57,6 +63,8 @@ namespace _5_Jahre_Hoelle.usercontrols
 
         private void Button_Exit_Click(object sender, RoutedEventArgs e)
         {
+            Logger.logger.Debug("button xit to Menu was pressed");
+
             // chatgpt anfang: wie mach ich das die menue page nicht im usercontrol aufgeht sondern in der main page
             Window.GetWindow(this).Content = new Frame
             {
@@ -64,11 +72,13 @@ namespace _5_Jahre_Hoelle.usercontrols
                 Content = new pages.menue_page()
             };
         }
-            // chatgpt ende
-        
+        // chatgpt ende
+
 
         private void Button_savegame_Click(object sender, RoutedEventArgs e)
         {
+            Logger.logger.Debug("button save Game was pressed");
+
             // chatgpt anfang: wie mach ich das die menue page nicht im usercontrol aufgeht sondern in der main page
             Window.GetWindow(this).Content = new Frame
             {
@@ -78,39 +88,47 @@ namespace _5_Jahre_Hoelle.usercontrols
             // chatgpt ende
             File.WriteAllText("save.json", _savedata);
             File.WriteAllText("GameTracer.txt", $"{GameTracer.LevelNumber}");
+
+            Logger.logger.Debug("Game was saved");
         }
 
         private void Button_Exit_MouseEnter(object sender, MouseEventArgs e)
         {
             polygon_exit.Opacity = 1;
+            Logger.logger.Debug("Mouse Entered Button Exit");
         }
 
         private void Button_Exit_MouseLeave(object sender, MouseEventArgs e)
         {
             polygon_exit.Opacity = 0
                 ;
+            Logger.logger.Debug("Mouse left Button Eit");
         }
 
         private void Button_savegame_MouseEnter(object sender, MouseEventArgs e)
         {
             polygon_Save.Opacity = 1
                 ;
+            Logger.logger.Debug("Mouse Entered Button save Game");
         }
 
         private void Button_savegame_MouseLeave(object sender, MouseEventArgs e)
         {
             polygon_Save.Opacity = 0
                 ;
+            Logger.logger.Debug("Mouse left Button save Game");
         }
 
         private void Button_continue_MouseEnter(object sender, MouseEventArgs e)
         {
             polygon_Continue.Opacity = 1;
+            Logger.logger.Debug("Mouse Entered Button continue");
         }
 
         private void Button_continue_MouseLeave(object sender, MouseEventArgs e)
         {
             polygon_Continue.Opacity = 0;
+            Logger.logger.Debug("Mouse left Button continue");
         }
     }
 }
